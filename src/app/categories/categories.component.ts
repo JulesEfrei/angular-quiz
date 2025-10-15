0da5a55797fe82ff821e1d0fb4c190020ea35c16
YounesEssl <younes.esslimani@efrei.net>
2025-10-15 10:23:18 +0200

    search bar for categories page

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -10,13 +10,30 @@ import { Category } from '../shared/models/category.model';
 })
 export class CategoriesComponent implements OnInit {
   categories: Category[] = [];
+  filteredCategories: Category[] = [];
+  searchTerm: string = '';
 
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe((data: Category[]) => {
       this.categories = data;
-      
+      this.filteredCategories = data;
     });
+  }
+
+  filterCategories(): void {
+    if (this.searchTerm.trim() === '') {
+      this.filteredCategories = this.categories;
+    } else {
+      this.filteredCategories = this.categories.filter(category =>
+        category.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+  }
+
+  clearSearch(): void {
+    this.searchTerm = '';
+    this.filteredCategories = this.categories;
   }
 }
